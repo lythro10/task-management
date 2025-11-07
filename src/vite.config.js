a74695b871b761011dae8cfffeb5b4222a83b14e
George Michael <george.michael45@gmail.com>
2025-11-07 10:13:31 +0200

    vite config to work on droplet

--- src/vite.config.js
+++ src/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         vue({
@@ -17,4 +17,26 @@ export default defineConfig({
             },
         }),
     ],
+    
+    
+    server: {
+        host: '0.0.0.0', 
+        port: 5173,      
+        
+        hmr: {
+            host: 'http://164.92.249.94/',
+            clientPort: 5173,
+            protocol: 'ws',
+        },
+        
+        // Optional: Explicitly allow the host application URL for development safety
+        cors: {
+            origin: [
+                'http://164.92.249.94:9002', // Your public IP and application port
+                'http://localhost:9002',       // Localhost for testing
+            ],
+            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+            credentials: true,
+        },
+    },
 });
